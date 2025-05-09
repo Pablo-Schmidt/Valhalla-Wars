813d681fdf67e351eef57775128aebdaaf453a1e
Pablo Schmidt <pablogschmidt2002@yahoo.com.ar>
2025-05-09 23:01:35 +0200

    Tracing Sword, showing LOG only
    Tracing Sword, showing LOG only

--- Source/ActionCombat/Public/Combat/TraceComponent.h
+++ Source/ActionCombat/Public/Combat/TraceComponent.h
@@ -12,6 +12,17 @@ class ACTIONCOMBAT_API UTraceComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	USkeletalMeshComponent* SkeletalComp;
+
+	UPROPERTY(EditAnywhere)
+	FName Start;
+
+	UPROPERTY(EditAnywhere)
+	FName End;
+
+	UPROPERTY(EditAnywhere)
+	FName Rotation;
+
 public:	
 	// Sets default values for this component's properties
 	UTraceComponent();
