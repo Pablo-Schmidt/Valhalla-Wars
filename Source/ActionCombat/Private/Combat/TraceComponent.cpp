fd85aa002757a18a04ed90ed22036fa4e35fda59
Pablo Schmidt <pablogschmidt2002@yahoo.com.ar>
2025-05-10 20:05:47 +0200

    Debugging a red and white shape
    Debugging a red and white shape to see if player is colliding with enemy

--- Source/ActionCombat/Private/Combat/TraceComponent.cpp
+++ Source/ActionCombat/Private/Combat/TraceComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Combat/TraceComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values for this component's properties
 UTraceComponent::UTraceComponent()
@@ -60,12 +62,22 @@ void UTraceComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActor
 		IgnoreParams
 	) };
 
-	if (bHasFoundTargets)
+	if (bDebugMode)
 	{
-		UE_LOG(
-			LogTemp,
-			Warning,
-			TEXT("Target Found!")
+		FVector CenterPoint{
+			UKismetMathLibrary::VLerp(
+				StartSocketLocation, EndSocketLocation, 0.5f
+			)
+		};
+
+		UKismetSystemLibrary::DrawDebugBox(
+			GetWorld(),
+			CenterPoint,
+			Box.GetExtent(),
+			bHasFoundTargets ? FLinearColor::Green : FLinearColor::Red,
+			ShapeRotation.Rotator(),
+			1.0f,
+			2.0f
 		);
 	}
 }
