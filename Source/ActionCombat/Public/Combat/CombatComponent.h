b2349fd4177824638a03d72d927c48813692691c
Pablo Schmidt <pablogschmidt2002@yahoo.com.ar>
2025-05-13 02:10:16 +0200

    Attack animation doesnt stop when re attacking
    Attack animation doesnt stop when re attacking, i can press attack, but first animation would be complete before switching to another animation

--- Source/ActionCombat/Public/Combat/CombatComponent.h
+++ Source/ActionCombat/Public/Combat/CombatComponent.h
@@ -7,12 +7,23 @@
 #include "CombatComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ACTIONCOMBAT_API UCombatComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+	UPROPERTY(EditAnywhere)
+	TArray<UAnimMontage*> AttackAnimations;
+
+	ACharacter* CharacterRef;
+
+	UPROPERTY(VisibleAnywhere)
+	int ComboCounter{ 0 };
+
+	UPROPERTY(VisibleAnywhere)
+	bool bCanAttack{ true };
+
+public:
 	// Sets default values for this component's properties
 	UCombatComponent();
 
@@ -20,9 +31,13 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	UFUNCTION(BlueprintCallable)
+	void ComboAttack();
+
+	UFUNCTION(BlueprintCallable)
+	void HandleResetAttack();
 };
